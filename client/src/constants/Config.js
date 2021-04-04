2dc2251af6fcd77dc3bec8eb11c125907641545c
Praveen Kumar <praveenscience@gmail.com>
2021-04-04 11:00:32 +0100

    Updated Praveen's Config.

--- client/src/constants/Config.js
+++ client/src/constants/Config.js
@@ -1,9 +1,8 @@
 const Config = {
   MainDetails: {
-    FullName: "Sneha Dudhankar",
-    Title: "Web Developer",
-    URL:
-      "https://media-exp1.licdn.com/dms/image/C5603AQEzFsMuZpxkAA/profile-displayphoto-shrink_800_800/0/1616475019891?e=1623283200&v=beta&t=OXzgSLHQcPjyxkdQAIIW5Z0Tbu0-6XT9ZZWpRHjTFV8",
+    FullName: "Praveen Kumar",
+    Title: "Full Stack JavaScript Developer",
+    URL: "https://i.imgur.com/5I70Obzm.png",
     Contacts: {
       Email: "sneha@web.com",
       WebSite: "https://www.web.com",
@@ -12,46 +11,48 @@ const Config = {
   },
   MainArea: {
     ProfileText:
-      "Hey, I am Sneha. I am a Full Stack Developer and a Digital Marketing Magician. I feel my best priority is to get the word about your brand out in the digital space. As I am also a Full Stack Developer, I am an engineer who can handle all the work of databases, servers, systems engineering, and clients. I am currently looking for new roles!",
+      "Hey, I am Praveen Kumar. I am a Full Stack Developer and a Cloud Computing Enthusiast. I feel my best priority is to get the word about your brand out in the digital space. As I am also a Full Stack Developer, I am an engineer who can handle all the work of databases, servers, systems engineering, and clients. I am not currently looking for new roles!",
     Works: [
       {
-        JobTitle: "Full Stack Developer at Dacway",
+        JobTitle: "Full Stack Developer at Google",
         Start: "Jan 2021",
         End: "Present",
         Description:
           "A full stack developer is an engineer who can handle all the work of databases, servers, systems engineering, and clients."
       },
       {
-        JobTitle: "Back End Developer at Equire IT Park",
-        Start: "Nov 2020",
+        JobTitle: "Front End Developer at Facebook",
+        Start: "Jan 2020",
         End: "Dec 2020",
         Description:
-          "Back-end developers are usually responsible for writing the web services and APIs used by front-end developers and mobile application developers."
+          "A front-end web developer is responsible for implementing visual elements that users see and interact with in a web application. They are usually supported by back-end web developers, who are responsible for server-side application logic and integration of the work front-end developers do."
       }
     ],
     Skills: [
-      "PHP",
       "HTML 5",
       "CSS 3",
       "JavaScript",
+      "jQuery",
       "ES6",
-      "Python",
-      "Flask",
+      "React JS",
+      "Node JS",
+      "Express JS",
+      "PHP",
       "MySQL",
       "postgreSQL"
     ],
     Studies: [
       {
-        Qualification: "Master of Computer Applications",
-        College: "Punyashlok Ahilyadevi Holkar Solapur University",
+        Qualification: "Masters in Cloud Computing",
+        College: "University of Leicester, England",
         Description:
-          "Master of Computer Applications or MCA is a professional degree in computer science. MCA is a three year long professional post-graduate degree course for students who deeply want to learn computer application development."
+          "This course focuses on the algorithmic basics and the software engineering principles and techniques underlying cloud computing. It equips students with the understanding and practical skills required by today's market."
       },
       {
-        Qualification: "Bachelors in Computer Application",
-        College: "Hirachand Nemchand College of Commerce",
+        Qualification: "Computer Science & Engineering",
+        College: "Anna University, Chennai",
         Description:
-          "A BCA degree is considered to be at par with a BTech degree in Computer Science or Information Technology. The degree helps interested students in setting up a sound academic base for an advanced career in Computer Applications."
+          "This course covers the basic theoretical aspects of Computer Software and Engineering with basics of engineering like Mechanical and Electrical Engineering with workshop experience."
       }
     ]
   }
