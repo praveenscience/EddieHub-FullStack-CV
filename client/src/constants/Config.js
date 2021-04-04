5b126922260114a7a2c0d94d4bbc0cd5856b1409
Praveen Kumar <praveenscience@gmail.com>
2021-04-04 10:34:19 +0100

    Completed the Work Experience for Sneha.

--- client/src/constants/Config.js
+++ client/src/constants/Config.js
@@ -12,7 +12,23 @@ const Config = {
   },
   MainArea: {
     ProfileText:
-      "Hey, I am Sneha. I am a Full Stack Developer and a Digital Marketing Magician. I feel my best priority is to get the word about your brand out in the digital space. As I am also a Full Stack Developer, I am an engineer who can handle all the work of databases, servers, systems engineering, and clients. I am currently looking for new roles!"
+      "Hey, I am Sneha. I am a Full Stack Developer and a Digital Marketing Magician. I feel my best priority is to get the word about your brand out in the digital space. As I am also a Full Stack Developer, I am an engineer who can handle all the work of databases, servers, systems engineering, and clients. I am currently looking for new roles!",
+    Works: [
+      {
+        JobTitle: "Full Stack Developer at Dacway",
+        Start: "Jan 2021",
+        End: "Present",
+        Description:
+          "A full stack developer is an engineer who can handle all the work of databases, servers, systems engineering, and clients."
+      },
+      {
+        JobTitle: "Back End Developer at Equire IT Park",
+        Start: "Nov 2020",
+        End: "Dec 2020",
+        Description:
+          "Back-end developers are usually responsible for writing the web services and APIs used by front-end developers and mobile application developers."
+      }
+    ]
   }
 };
 
