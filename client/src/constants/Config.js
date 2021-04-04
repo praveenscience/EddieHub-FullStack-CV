eb729e3be186ac07202de838f7fbe8fc5aa3b572
Praveen Kumar <praveenscience@gmail.com>
2021-04-04 10:23:39 +0100

    Added MainArea in Config & Updated Content.

--- client/src/constants/Config.js
+++ client/src/constants/Config.js
@@ -3,12 +3,16 @@ const Config = {
     FullName: "Sneha Dudhankar",
     Title: "Web Developer",
     URL:
-      "https://yt4.ggpht.com/ytc/AAUvwnjlHIJMMLHYwJ5qt2JCMd2C7qgJebPxPD2GGrqKVg=c-k-c0x00ffffff-no-rj",
+      "https://media-exp1.licdn.com/dms/image/C5603AQEzFsMuZpxkAA/profile-displayphoto-shrink_800_800/0/1616475019891?e=1623283200&v=beta&t=OXzgSLHQcPjyxkdQAIIW5Z0Tbu0-6XT9ZZWpRHjTFV8",
     Contacts: {
       Email: "sneha@web.com",
       WebSite: "https://www.web.com",
       Phone: "+44 789 456 1230"
     }
+  },
+  MainArea: {
+    ProfileText:
+      "Hey, I am Sneha. I am a Full Stack Developer and a Digital Marketing Magician. I feel my best priority is to get the word about your brand out in the digital space. As I am also a Full Stack Developer, I am an engineer who can handle all the work of databases, servers, systems engineering, and clients. I am currently looking for new roles!"
   }
 };
 
