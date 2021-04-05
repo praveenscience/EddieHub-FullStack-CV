6f22ef5be478afaccb8ec07e578fa353a504a6af
Praveen Kumar <praveenscience@gmail.com>
2021-04-05 08:07:19 +0100

    Added Empty Config JSON.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,10 @@
-import Config from "../constants/Config";
+import DefaultConfig from "../constants/Config";
+import UserConfig from "../constants/Config.json";
 import MainArea from "./MainArea/MainArea";
 import MainDetails from "./MainDetails/MainDetails";
 
 const App = () => {
+  const Config = UserConfig ? UserConfig : DefaultConfig;
   document.title = Config.MainDetails.FullName + " - Curriculum Vitae";
   return (
     <>
